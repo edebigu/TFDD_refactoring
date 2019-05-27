6b997c5691f3c286aae82cf8e919d625235163c5
edebigu <debora.iniguez@gmail.com>
2019-05-27 18:16:38 +0200

    v2: borrada la herencia de Foundation

--- src/main/java/klondike/models/Foundation.java
+++ src/main/java/klondike/models/Foundation.java
@@ -1,11 +1,14 @@
 package klondike.models;
 
-public class Foundation extends CardStack {
+import java.util.Stack;
+
+public class Foundation  {
 
     private Suit suit;
+    protected Stack<Card> cards;
 
     public Foundation(Suit suit) {
-        super();
+    	this.cards = new Stack<Card>();
         this.suit = suit;
     }
 
@@ -22,5 +25,21 @@ public class Foundation extends CardStack {
 
     public Suit getSuit() {
         return this.suit;
+    }
+    
+    public boolean empty() {
+        return this.cards.empty();
+    }
+    
+    public Card peek() {
+        return this.cards.peek();
+    }
+    
+    public void push(Card card) {
+        this.cards.push(card);
+    }
+    
+    public Card pop() {
+        return this.cards.pop();
     }
 }
